a8a268f819cce38c31711deb154e0af9c4640308
Franck Silvestre <franck.silvestre@gmail.com>
2024-03-13 14:51:12 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,45 +1,96 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+/**
+ * An enterprise is a company that is a project partner.
+ *
+ */
+@Entity
 public class Enterprise {
     @NotEmpty private String name;
     @Size(min = 10) private String description;
     @NotEmpty private String contactName;
     @NotEmpty @Email
     private String contactEmail;
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    /**
+     * Set the name of the enterprise
+     * @param name the name of the enterprise
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the name of the enterprise
+     * @return the name of the enterprise
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the description of the enterprise
+     * @param description the description of the enterprise
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the description of the enterprise
+     * @return the description of the enterprise
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set the contact name of the enterprise
+     * @param contactName the contact name of the enterprise
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * Get the contact name of the enterprise
+     * @return the contact name of the enterprise
+     */
     public String getContactName() {
         return contactName;
     }
 
+    /**
+     * Set the contact email of the enterprise
+     * @param contactEmail the contact email of the enterprise
+     */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
 
+    /**
+     * Get the contact email of the enterprise
+     * @return the contact email of the enterprise
+     */
     public String getContactEmail() {
         return contactEmail;
+    }
+
+    /**
+     * Get the id of the enterprise
+     * @return the id of the enterprise
+     */
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,25 +1,60 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
+/**
+ * A project is a business idea that an entrepreneur wants to develop.
+ *
+ */
+@Entity
 public class Project {
     @NotEmpty
     private String title;
     private String description;
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    /**
+     * Set the title of the project
+     * @param title the title of the project
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the title of the project
+     * @return the title of the project
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the description of the project
+     * @param description the description of the project
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the description of the project
+     * @return the description of the project
+     */
     public String getDescription() {
         return description;
+    }
+
+    /**
+     * Get the id of the project
+     * @return the id of the project
+     */
+    public Long getId() {
+        return id;
     }
 }
